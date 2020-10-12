509dc2406779715e1feea828a0e45f7fe2579da1
daniel21h <hesseldaniel21@gmail.com>
2020-10-12 09:38:59 -0300

    feat: create landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { FiArrowRight } from "react-icons/fi";
+
+import "./styles/global.css";
+import "./styles/pages/landing.css";
+
+import logoImg from "./images/logo.svg";
 
 function App() {
-  return <h1>Hello Omnistack</h1>;
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Embu-Guaçu</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href="/enter-app" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
